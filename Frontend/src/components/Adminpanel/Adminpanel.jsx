521e388ab2c26da8b5103bcc8b0af45d284aef6e
Mohamadhasn SJ <mohamadhasan.synjli@gmail.com>
2025-09-30 18:57:26 +0330

    (Frontend) admin panel design is finished

--- Frontend/src/components/Adminpanel/Adminpanel.jsx
+++ Frontend/src/components/Adminpanel/Adminpanel.jsx
@@ -1,3 +1,4 @@
+import Clock from '../Clock/Clock';
 import Usercard from '../Usercard/Usercard';
 
 import style from './Adminpanel.module.css';
@@ -7,30 +8,31 @@ export default function Adminpanel() {
 		<div className={style.container}>
 			<div className={style.dataContainer}>
 				<div className={style.whiteSpace}></div>
-				<div className={style.dataPanel}></div>
+				<div className={style.dataPanel}>
+					<Clock />
+					<div className={style.data}>
+						<p>Task</p>
+						<p>Users</p>
+					</div>
+				</div>
 				<div className={style.whiteSpace}></div>
 			</div>
 			<div className={style.userContainer}>
-				<div className={style.userList}>
-					<Usercard />
-					<Usercard />
-					<Usercard />
-					<Usercard />
-					<Usercard />
-					<Usercard />
-					<Usercard />
-					<Usercard />
-					<Usercard />
-					<Usercard />
-					<Usercard />
-					<Usercard />
-					<Usercard />
-					<Usercard />
-					<Usercard />
-					<Usercard />
-					<Usercard />
+				<div className={style.userListContainer}>
+					<div className={style.userList}>
+						<Usercard />
+						<Usercard />
+						<Usercard />
+						<Usercard />
+						<Usercard />
+						<Usercard />
+						<Usercard />
+					</div>
+					<div className={style.paging}>{/* page */}</div>
 				</div>
-				<div className={style.userEdit}></div>
+				<div className={style.userEdit}>
+					<h4>Check back soon..</h4>
+				</div>
 			</div>
 		</div>
 	);
--- Frontend/src/components/Usercard/Usercard.jsx
+++ Frontend/src/components/Usercard/Usercard.jsx
@@ -1,15 +1,15 @@
 import style from './Usercard.module.css';
 
+import userIcon from '../../assets/userIcon.jpg';
+
 export default function Usercard() {
 	return (
 		<div className={style.container}>
-			<p>1</p>
-			<p>2</p>
-			<p>3</p>
-
-			{/* image */}
-			{/* user name */}
-			{/* edit btn or icon */}
+			<div className={style.userInfo}>
+				<img src={userIcon} alt="usericon" className={style.userIcon} />
+				<h3>Mali</h3>
+			</div>
+			<i>Edit</i>
 		</div>
 	);
 }
