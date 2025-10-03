0b217685c219d7d9fcf312eaeddd7fecf84f73ac
Mohamadhasn SJ <mohamadhasan.synjli@gmail.com>
2025-10-03 21:59:37 +0330

    (Fronend) leftover commits

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,3 +1,8 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import { useState, useEffect, createContext } from 'react';
+
+import { jwtDecode } from 'jwt-decode';
+
 import Signin from './components/Signin/Signin';
 import Userpanel from './components/Userpanel/Userpanel';
 import Adminpanel from './components/Adminpanel/Adminpanel';
@@ -5,11 +10,36 @@ import Adminpanel from './components/Adminpanel/Adminpanel';
 import './App.css';
 
 function App() {
+	const [isLogedIn, setIsLogedIn] = useState(false);
+	const [isAdmin, setIsAdmin] = useState(false);
+
+	useEffect(() => {
+		const token = localStorage.getItem('token');
+		if (token) {
+			const decoded = jwtDecode(token);
+			const currentTime = Date.now() / 1000;
+			if (decoded.exp < currentTime) {
+				localStorage.removeItem('token');
+				localStorage.removeItem('user');
+				setIsLogedIn(false);
+				setIsAdmin(false);
+			} else {
+				const user = JSON.parse(localStorage.getItem('user'));
+				setIsLogedIn(true);
+				setIsAdmin(user.isAdmin);
+			}
+		}
+	}, []);
+
 	return (
 		<>
-			{/* <Signin /> */}
-			<Userpanel />
-			{/* <Adminpanel /> */}
+			{!isLogedIn ? (
+				<Signin setIsLogedIn={setIsLogedIn} setIsAdmin={setIsAdmin} />
+			) : isAdmin ? (
+				<Adminpanel setIsLogedIn={setIsLogedIn} setIsAdmin={setIsAdmin} />
+			) : (
+				<Userpanel setIsLogedIn={setIsLogedIn} setIsAdmin={setIsAdmin} />
+			)}
 		</>
 	);
 }
