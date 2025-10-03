2f7d2a836cfa1e88ea74f032e41a9e55ee2991af
Mohamadhasn SJ <mohamadhasan.synjli@gmail.com>
2025-10-03 21:52:34 +0330

    (Frontend) user dashboard

--- Frontend/src/components/Dashboard/Dashboard.jsx
+++ Frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,45 @@
-export default function Dashboard() {
+import style from '../Login/Login.module.css';
+import styles from '../Usercard/Usercard.module.css';
+import styless from './Dashboard.module.css';
+
+export default function Dashboard({ userData, setUserData }) {
 	return (
-		<div>
-			<h1>This is the dashboard for now!!</h1>
+		<div className={style.input}>
+			<input
+				type="file"
+				accept="image/*"
+				onChange={(e) =>
+					setUserData((prev) => ({
+						...prev,
+						image: e.target.files[0],
+					}))
+				}
+			/>
+
+			<label>Username</label>
+			<input type="text" value={userData.userName} disabled={true} />
+			<label>E-mail</label>
+			<input
+				type="email"
+				value={userData.email}
+				onChange={(e) =>
+					setUserData((prev) => ({
+						...prev,
+						email: e.target.value,
+					}))
+				}
+			/>
+			<label>Set new Password</label>
+			<input
+				type="Password"
+				value={userData.newPassword}
+				onChange={(e) =>
+					setUserData((prev) => ({
+						...prev,
+						newPassword: e.target.value,
+					}))
+				}
+			/>
 		</div>
 	);
 }
