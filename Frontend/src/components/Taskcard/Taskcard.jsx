bcf4b885bdfe4790d541f27b1f71aafacf56adab
Mohamadhasn SJ <mohamadhasan.synjli@gmail.com>
2025-10-03 21:56:56 +0330

    (Fronend) task panel logic and dynamic data

--- Frontend/src/components/Taskcard/Taskcard.jsx
+++ Frontend/src/components/Taskcard/Taskcard.jsx
@@ -1,21 +1,25 @@
 import style from './Taskcard.module.css';
 
-export default function Taskcard() {
+export default function Taskcard({
+	title,
+	decription,
+	createdAt,
+	handleDelete,
+	taskId,
+}) {
 	return (
 		<div className={style.container}>
 			<div id="title" className={style.head}>
-				<h3>Tst</h3>
-			</div>
-			<div id="body" className={style.body}>
-				<p>
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel
-					exercitationem magnam accusamus similique earum corrupti, in hic ipsa,
-					sint nam, nihil soluta omnis porro cupiditate dignissimos dicta nemo
-					totam ea.
+				<h3>{title}</h3>
+				<p onClick={() => handleDelete(taskId)} className={style.delete}>
+					X
 				</p>
 			</div>
+			<div id="body" className={style.body}>
+				<p>{decription}</p>
+			</div>
 			<div id="date" className={style.date}>
-				date
+				{createdAt}
 			</div>
 		</div>
 	);
--- Frontend/src/components/Taskpanel/Taskpanel.jsx
+++ Frontend/src/components/Taskpanel/Taskpanel.jsx
@@ -1,19 +1,100 @@
+import { useEffect, useState } from 'react';
+
 import Taskcard from '../Taskcard/Taskcard';
 import style from './Taskpanel.module.css';
 
-export default function Taskpanel() {
+export default function Taskpanel({
+	setIsLogedIn,
+	setIsAdmin,
+	setReload,
+	reload,
+}) {
+	const [tasks, setTasks] = useState([]);
+	const [page, setPage] = useState(1);
+	const [totalPages, setTotalPages] = useState(1);
+
+	const user = JSON.parse(localStorage.getItem('user'));
+	const userId = user.userId;
+	const token = localStorage.getItem('token');
+
+	function handleDelete(taskId) {
+		fetch('http://localhost:3030/deleteTask/' + taskId, {
+			method: 'DELETE',
+			headers: { Authorization: token },
+		})
+			.then((res) => {
+				setReload((prev) => !prev);
+				return res.json();
+			})
+			.then((data) => {})
+			.catch();
+	}
+
+	useEffect(() => {
+		fetch('http://localhost:3030/userTask?page=' + page, {
+			headers: {
+				Authorization: token,
+			},
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				setTasks(data.tasks);
+				setTotalPages(data.totalPages);
+			})
+			.catch((err) => {
+				console.error('Error fetching tasks:', err);
+			});
+	}, [page, userId, reload]);
+
+	function handleLogOut() {
+		console.log(user);
+		localStorage.removeItem('token');
+		localStorage.removeItem('user');
+		setIsLogedIn(false);
+		setIsAdmin(false);
+	}
+
 	return (
 		<div className={style.container}>
-			<div className={style.whiteSpace}></div>
-			<div className={style.taskContainer}>
-				<Taskcard />
-				<Taskcard />
-				<Taskcard />
-				<Taskcard />
-				<Taskcard />
-				<Taskcard />
+			<div className={style.whiteSpace}>
+				<button className={style.btn} onClick={handleLogOut}>
+					Logout
+				</button>
 			</div>
-			<div className={style.pagination}></div>
+			<div className={style.taskContainer}>
+				{tasks.map((item) => (
+					<Taskcard
+						key={item.id}
+						title={item.title}
+						decription={item.decription}
+						createdAt={item.createdAt}
+						taskId={item.id}
+						handleDelete={handleDelete}
+					/>
+				))}
+			</div>
+			<div className={style.pagination}>
+				<button
+					className={style.btnp}
+					onClick={() => setPage((p) => Math.max(p - 1, 1))}
+					disabled={page === 1}
+				>
+					Prev
+				</button>
+
+				<span className={style.pages}>
+					{' '}
+					Page {page} of {totalPages}{' '}
+				</span>
+
+				<button
+					className={style.btnp}
+					onClick={() => setPage((p) => Math.min(p + 1, totalPages))}
+					disabled={page === totalPages}
+				>
+					Next
+				</button>
+			</div>
 		</div>
 	);
 }
