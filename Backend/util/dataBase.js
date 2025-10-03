f6de2b3624519ab489b0acdc1dff5bed584d3706
Mohamadhasn SJ <mohamadhasan.synjli@gmail.com>
2025-10-03 22:09:22 +0330

    (Backend) database connection instructions

--- Backend/util/dataBase.js
+++ Backend/util/dataBase.js
@@ -1,11 +1,17 @@
 const { Sequelize } = require('sequelize');
 
+//ONLY EDIT THE THREE VARIABLES
+
 const dataBaseName = 'task-manager';
+// add you data base name instead of the task-manager
 
 const dataBaseUsername = 'root';
+// data base username, this option can be viewed in MySQL work bench
 
 const dataBasePassword = '123456789';
+// Data password that was give to the database in installation
 
+// DO NOT TOUCH THE CODE HERE
 const sequelize = new Sequelize(
 	dataBaseName,
 	dataBaseUsername,
