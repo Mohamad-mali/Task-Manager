c5d76d46e25d383a1fced70819d3796c18abfa92
Mohamadhasn SJ <mohamadhasan.synjli@gmail.com>
2025-10-03 21:58:10 +0330

    (Frontend) userpanel and user card logic, usercard is now universal

--- Frontend/src/components/Usercard/Usercard.jsx
+++ Frontend/src/components/Usercard/Usercard.jsx
@@ -2,14 +2,44 @@ import style from './Usercard.module.css';
 
 import userIcon from '../../assets/userIcon.jpg';
 
-export default function Usercard() {
+export default function Usercard({
+	setIsEditing = () => {},
+	isEditing = false,
+	handleSave = () => {},
+	inadmin = false,
+	userData,
+	handleEditAdmin = () => {},
+}) {
+	function handleEdit() {
+		setIsEditing(true);
+	}
+
 	return (
 		<div className={style.container}>
 			<div className={style.userInfo}>
-				<img src={userIcon} alt="usericon" className={style.userIcon} />
-				<h3>Mali</h3>
+				<img
+					src={`http://localhost:3030${userData.image}`}
+					alt="usericon"
+					className={style.userIcon}
+				/>
+				<h3 className={style.user}>{userData.userName}</h3>
 			</div>
-			<i>Edit</i>
+			{inadmin ? (
+				<button
+					className={style.btnA}
+					onClick={() => handleEditAdmin(userData.id)}
+				>
+					Edit
+				</button>
+			) : isEditing ? (
+				<button className={style.btn} onClick={handleSave}>
+					Save
+				</button>
+			) : (
+				<button className={style.btn} onClick={handleEdit}>
+					Edit
+				</button>
+			)}
 		</div>
 	);
 }
--- Frontend/src/components/Userpanel/Userpanel.jsx
+++ Frontend/src/components/Userpanel/Userpanel.jsx
@@ -1,13 +1,21 @@
+import { useState } from 'react';
+
 import Sidepanle from '../Sidepanel/Sidepanel.jsx';
 import Taskpanel from '../Taskpanel/Taskpanel.jsx';
 
 import style from './Userpanel.module.css';
 
-export default function Userpanel() {
+export default function Userpanel({ setIsLogedIn, setIsAdmin }) {
+	const [reload, setReload] = useState(false);
 	return (
 		<div className={style.container}>
-			<Sidepanle />
-			<Taskpanel />
+			<Sidepanle setReload={setReload} reload={reload} />
+			<Taskpanel
+				setIsLogedIn={setIsLogedIn}
+				setIsAdmin={setIsAdmin}
+				setReload={setReload}
+				reload={reload}
+			/>
 		</div>
 	);
 }
