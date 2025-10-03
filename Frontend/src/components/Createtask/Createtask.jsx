952c11b35d7dd69c17c56830a8c6119c291d1c52
Mohamadhasn SJ <mohamadhasan.synjli@gmail.com>
2025-10-03 21:53:22 +0330

    (Frontend) task creation api and logic

--- Frontend/src/components/Createtask/Createtask.jsx
+++ Frontend/src/components/Createtask/Createtask.jsx
@@ -2,14 +2,39 @@ import { useState } from 'react';
 
 import style from './Createtask.module.css';
 
-export default function createtask() {
+export default function createtask({ setReload }) {
 	const [creatingTask, setCreatingTask] = useState(false);
+	const [title, setTitle] = useState('');
+	const [description, setDescription] = useState('');
 
 	const createtask = () => {
 		setCreatingTask((priv) => !priv);
 	};
 
-	function handleSave() {}
+	function handleSave() {
+		const user = JSON.parse(localStorage.getItem('user'));
+		const userId = user.userId;
+		const token = localStorage.getItem('token');
+
+		fetch('http://localhost:3030/createTask', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: token,
+			},
+			body: JSON.stringify({ title, description, userId }),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				setReload((prev) => !prev);
+				setTitle('');
+				setDescription('');
+				setCreatingTask(false);
+			})
+			.catch((err) => {
+				console.log('Error creating task:', err);
+			});
+	}
 	return (
 		<div className={style.taskContainer}>
 			<button className={style.ctBtn} onClick={createtask}>
@@ -19,12 +44,17 @@ export default function createtask() {
 			<div className={style.inputContainer} hidden={!creatingTask}>
 				<div className={style.inputs}>
 					<label>Task Title</label>
-					<input type="text" />
+					<input
+						type="text"
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+					/>
 					<label>Description</label>
-					<textarea className={style.textarea}></textarea>
-					<label>Color</label>
-					<input type="color" />
-					{/* make 5 btn to chose between 5 pre set colors */}
+					<textarea
+						className={style.textarea}
+						value={description}
+						onChange={(e) => setDescription(e.target.value)}
+					></textarea>
 				</div>
 
 				<div className={style.btnContainer}>
