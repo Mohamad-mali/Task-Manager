49a896deb00fbd7e3f2a5220c2f872b19c4c9d83
Mohamadhasn SJ <mohamadhasan.synjli@gmail.com>
2025-10-03 21:55:00 +0330

    (Frontend) loging logic and state plus api connection

--- Frontend/src/components/Login/Login.jsx
+++ Frontend/src/components/Login/Login.jsx
@@ -1,23 +1,60 @@
 import style from './Login.module.css';
+import { useState } from 'react';
 
-export default function Login({ accState }) {
+export default function Login({ accState, setIsLogedIn, setIsAdmin }) {
+	const [password, setPassword] = useState('');
+	const [userName, setUserName] = useState('');
+
 	const createAcc = () => {
 		return accState(false);
+	};
+
+	const handleLogin = () => {
+		fetch('http://localhost:3030/login', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ password, userName }),
+		})
+			.then((res) => {
+				if (res.status !== 200) {
+					throw new Error();
+				} else {
+					return res.json();
+				}
+			})
+			.then((data) => {
+				localStorage.setItem('token', data.token);
+				localStorage.setItem('user', JSON.stringify(data.user));
+				setIsLogedIn(true);
+				setIsAdmin(data.user.isAdmin);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	};
 
 	return (
 		<>
 			<h2 className={style.tt}>Log In</h2>
-
 			<div className={style.input}>
 				<label>Username</label>
-				<input type="text" />
+				<input
+					type="text"
+					value={userName}
+					onChange={(e) => setUserName(e.target.value)}
+				/>
 				<label>Password</label>
-				<input type="Password" />
+				<input
+					type="Password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
 			</div>
 
 			<div className={style.btnContainer}>
-				<button className={style.btn}>LogIn</button>
+				<button onClick={handleLogin} className={style.btn}>
+					LogIn
+				</button>
 				<a className={style.aLink} onClick={createAcc}>
 					Create an account!
 				</a>
